76e359068d0e557ea4115a6976c12b90f6c664ad
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2017-04-21 17:18:52 -0700

    Chain Code

--- test/chaincode_start.go
+++ test/chaincode_start.go
@@ -287,8 +287,14 @@ func validateEvent(contractInfo string, eventInfo string) bool {
 }
 
 func (t *SimpleChaincode) validate(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
+	//var key, jsonResp string
+	//var err error
+
+	type ValidateResponse struct {
+		status string `json:"status"`
+	}
+
+	var validateResponse ValidateResponse
 
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
@@ -301,23 +307,38 @@ func (t *SimpleChaincode) validate(stub shim.ChaincodeStubInterface, args []stri
 
 	fmt.Println("\n\n productType ------------------------- " + productType)
 
-	valAsbytes, err := stub.GetState(productType)
+	valAsbytes, _ := stub.GetState(productType)
 
 	// n := bytes.IndexByte(valAsbytes, 0)
 	// contractInfo := string(valAsbytes[:n])
 
 	contractInfo := string(valAsbytes[:])
+
 	fmt.Println("\n\n Contract Retrieved " + contractInfo)
 
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+	ret := validateEvent(eventInfo, contractInfo)
 
-		return nil, errors.New(jsonResp)
+	if ret == true {
+
+		validateResponse.status = "success"
+
+		validateResponseAsBytes, _ := json.Marshal(validateResponse) //convert to array of bytes
+		return validateResponseAsBytes, nil
 	}
+	validateResponse.status = "failure"
 
-	fmt.Println(valAsbytes)
+	validateResponseAsBytes, _ := json.Marshal(validateResponse) //convert to array of bytes
+	return validateResponseAsBytes, nil
 
-	return nil, nil
+	// if err != nil {
+	// 	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+	//
+	// 	return nil, errors.New(jsonResp)
+	// }
+	//
+	// fmt.Println(valAsbytes)
+	//
+	// return nil, nil
 
 }
 
